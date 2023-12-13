be5a26dd7c888f467b3c55e633b4116f6a76ee1b
ratanmamdekar <39640658+ratanmamdekar@users.noreply.github.com>
2023-12-13 11:11:45 +0530

    day13

---
+++ AdventOfCode2023/src/main/java/Day13Part2.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Day13Part2 {
+
+    static Map<Character,int[]> moves;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter all tile strings: ");
+        String str = sc.nextLine();
+        List<String> lavaIslands = new ArrayList<>();
+
+        while (!str.equalsIgnoreCase("end")) {
+            lavaIslands.add(str);
+            str = sc.nextLine();
+        }
+
+        int ans = process(lavaIslands);
+        System.out.println("Summary all of your notes: " + ans);
+    }
+
+    private static int process(List<String> lavaIslands) {
+        List<String> lavaIsland = new ArrayList<>();
+        int ans=0;
+        for (String string : lavaIslands){
+            if(string.isBlank()){
+                int temp = getSummary(lavaIsland);
+                System.out.println("temp: "+temp);
+                lavaIsland = new ArrayList<>();
+                ans+=temp;
+            }else {
+                lavaIsland.add(string);
+            }
+        }
+
+        int temp = getSummary(lavaIsland);
+        System.out.println("temp: "+temp);
+        ans+=temp;
+
+        return ans;
+    }
+
+    private static int getSummary(List<String> lavaIsland) {
+        //check if symmetry is along row
+        int rowMirror = getRowMirror(lavaIsland);
+        if(rowMirror!=-1){
+            return rowMirror*100;
+        }
+
+        //check if symmetry is along col
+        return getColMirror(lavaIsland);
+    }
+
+    private static int getRowMirror(List<String> lavaIsland) {
+        for (int i=0;i<lavaIsland.size()-1;i++){
+            if (symmetryAlongRow(lavaIsland,i)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean symmetryAlongRow(List<String> lavaIsland, int idx) {
+        int mismatch = 0;
+        for (int i1=idx,i2=idx+1;i1>=0 && i2<lavaIsland.size();i1--, i2++){
+            for (int j=0;j<lavaIsland.get(0).length();j++){
+                if (lavaIsland.get(i1).charAt(j)!= lavaIsland.get(i2).charAt(j))
+                    mismatch++;
+
+                if (mismatch>1){
+                    return false;
+                }
+            }
+
+        }
+        return mismatch==1;
+    }
+
+    private static int getColMirror(List<String> lavaIsland) {
+        for (int j=0;j<lavaIsland.get(0).length();j++){
+            if (symmetryAlongCol(lavaIsland,j)){
+                return j+1;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean symmetryAlongCol(List<String> lavaIsland, int idx) {
+        int mismatch = 0;
+        for (int j1=idx,j2=idx+1;j1>=0 && j2<lavaIsland.get(0).length();j1--, j2++){
+            for (String string : lavaIsland) {
+                if (string.charAt(j1) != string.charAt(j2))
+                    mismatch++;
+                if (mismatch>1){
+                    return false;
+                }
+            }
+        }
+        return mismatch==1;
+    }
+
+}
+
+/*\
+sample input
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+end
+
+ans - 405
+* */
